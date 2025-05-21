6847ce76ec7e2226d1e4946c0f06e70fde79bc69
Kiloson <4closetool3@gmail.com>
2025-05-21 03:25:16 +0000

    Update TUN examples with IPv6 support and address changes
    Refactor `tun-bridge` and `tun` examples to handle both IPv4 and IPv6 packets. Update the IP address and netmask configurations. Remove redundant `tun-route` example.

--- examples/tun-bridge/tun.rs
+++ examples/tun-bridge/tun.rs
@@ -1,36 +1,22 @@
 use {
-    etherparse::{Ipv4Slice, UdpHeader},
+    etherparse::{Ipv4Slice, Ipv6Slice, UdpHeader},
     tokio::{
         net::UdpSocket,
-        time::{self, Duration},
     },
 };
 
 #[tokio::main]
 async fn main() {
-    // tokio::spawn(async {
-        tun_listen().await;
-    // });
-
-    // TODO: add route from 10.0.199.0/24 to 10.0.200.9
-}
-
-async fn send() {
-    // send data to 10.0.200.9
-    let socket = UdpSocket::bind("0.0.0.0:0").await.unwrap();
-    socket
-        .send_to(b"Hello, world!", "10.0.200.9:80")
-        .await
-        .unwrap();
+    tun_listen().await;
 }
 
 async fn tun_listen() {
+    println!("Listen on 10.200.9.1");
     let mut config = tun::Configuration::default();
     config
-        .address((10, 0, 200, 9))
-        .netmask((255, 255, 255, 0))
-        .destination((10, 0, 200, 255))
-        .mtu(1500)
+        .address((10, 200, 9, 1))
+        .netmask((255, 255, 0, 0))
+        // .destination((10, 0, 200, 1))
         .up();
 
     let dev = tun::create_as_async(&config).unwrap();
@@ -38,9 +24,19 @@ async fn tun_listen() {
 
     loop {
         let amount = dev.recv(buf.as_mut()).await.unwrap();
-        let slice = Ipv4Slice::from_slice(&buf[..amount]).unwrap();
-        let (udp_header, payload) = UdpHeader::from_slice(slice.payload().payload).unwrap();
-        let content = String::from_utf8_lossy(payload);
-        println!("Received: {:?}, {:?}", udp_header, content);
+        println!("Received {} bytes", amount);
+
+        if let Ok(slice) = Ipv6Slice::from_slice(&buf[..amount]) {
+            if let Ok((udp_header, payload)) = UdpHeader::from_slice(slice.payload().payload) {
+                let content = String::from_utf8_lossy(payload);
+                println!("Received: {:?}, {:?}", udp_header, content);
+            }
+        }
+        if let Ok(slice) = Ipv4Slice::from_slice(&buf[..amount]) {
+            if let Ok((udp_header, payload)) = UdpHeader::from_slice(slice.payload().payload) {
+                let content = String::from_utf8_lossy(payload);
+                println!("Received: {:?}, {:?}", udp_header, content);
+            }
+        }
     }
 }
--- examples/tun/tun.rs
+++ examples/tun/tun.rs
@@ -1,5 +1,5 @@
 use {
-    etherparse::{Ipv4Slice, UdpHeader},
+    etherparse::{Ipv4Slice, Ipv6Slice, UdpHeader},
     tokio::{
         net::UdpSocket,
         time::{self, Duration},
@@ -12,25 +12,29 @@ async fn main() {
         tun_listen().await;
     });
 
-    time::sleep(Duration::from_secs(1)).await;
-    send().await;
-    time::sleep(Duration::from_secs(1)).await;
+    for _ in 0..10 {
+        time::sleep(Duration::from_secs(1)).await;
+        send().await;
+    }
+    // time::sleep(Duration::from_secs(100)).await;
 }
 
 async fn send() {
     // send data to 10.0.200.9
     let socket = UdpSocket::bind("0.0.0.0:0").await.unwrap();
     socket
-        .send_to(b"Hello, world!", "10.0.200.9:80")
+        .send_to(b"Hello, world!", "10.200.9.1:80")
         .await
         .unwrap();
+    println!("Sent data to 10.200.9.1:80")
 }
 
 async fn tun_listen() {
+    println!("Listen on 10.200.9.1");
     let mut config = tun::Configuration::default();
     config
-        .address((10, 0, 200, 9))
-        .netmask((255, 255, 255, 0))
+        .address((10, 200, 9,1))
+        .netmask((255, 255, 0, 0))
         // .destination((10, 0, 200, 1))
         .up();
 
@@ -39,9 +43,19 @@ async fn tun_listen() {
 
     loop {
         let amount = dev.recv(buf.as_mut()).await.unwrap();
-        let slice = Ipv4Slice::from_slice(&buf[..amount]).unwrap();
-        let (udp_header, payload) = UdpHeader::from_slice(slice.payload().payload).unwrap();
-        let content = String::from_utf8_lossy(payload);
-        println!("Received: {:?}, {:?}", udp_header, content);
+        println!("Received {} bytes", amount);
+
+        if let Ok(slice) = Ipv6Slice::from_slice(&buf[..amount]) {
+            if let Ok((udp_header, payload)) = UdpHeader::from_slice(slice.payload().payload) {
+                let content = String::from_utf8_lossy(payload);
+                println!("Received: {:?}, {:?}", udp_header, content);
+            }
+        }
+        if let Ok(slice) = Ipv4Slice::from_slice(&buf[..amount]) {
+            if let Ok((udp_header, payload)) = UdpHeader::from_slice(slice.payload().payload) {
+                let content = String::from_utf8_lossy(payload);
+                println!("Received: {:?}, {:?}", udp_header, content);
+            }
+        }
     }
 }
